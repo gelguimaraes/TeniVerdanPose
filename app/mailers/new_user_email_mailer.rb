8f83846963c08ddf52d31be1ed8d003340217a85
Gesoaldo <gesoaldo.guimaraes@academico.ifpb.edu.br>
2019-08-11 22:41:14 -0300

    mailer

--- app/mailers/new_user_email_mailer.rb
+++ app/mailers/new_user_email_mailer.rb
@@ -1,10 +1,12 @@
 class NewUserEmailMailer < ApplicationMailer
+
   def mail_to_indicated(indicator, indicated, movie, plataforma)
     @indicator = indicator
     @indicated = indicated
     @movie = movie
     @plataforma = plataforma
     @base_url = "https://teniverdanpose.herokuapp.com/"
-    mail(to: @indicated.email, subject: ('Seu amigo ' + @indicator.nome + ' te indicou um filme' ))
+    mail(from: @indicator.email, to: @indicated.email,
+         subject: ('Seu amigo ' + @indicator.nome + ' te indicou um filme' ))
   end
 end
