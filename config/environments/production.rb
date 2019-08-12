85e59144e68b0681abdf19b8b5550821baba126a
Gesoaldo <gesoaldo.guimaraes@academico.ifpb.edu.br>
2019-08-11 22:01:46 -0300

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,9 +72,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
       port:                 587,
-      domain:               ENV["GMAIL_DOMAIN"],
-      user_name:            ENV["GMAIL_USERNAME"],
-      password:             ENV["GMAIL_PASSWORD"],
+      domain:               'gmail.com',
+      user_name:            'teniverdanpose@gmail.com',
+      password:             'tvpsmtpvo9793',
       authentication:       'plain',
       enable_starttls_auto: true,
       openssl_verify_mode: "none"
