367c7f78a688fdebce40e26d5f8b2c2611035379
Gesoaldo <gesoaldo.guimaraes@academico.ifpb.edu.br>
2019-08-11 22:22:32 -0300

    mailer

--- app/mailers/new_user_email_mailer.rb
+++ app/mailers/new_user_email_mailer.rb
@@ -1,12 +1,11 @@
 class NewUserEmailMailer < ApplicationMailer
-
+  default :from => @indicator.email
   def mail_to_indicated(indicator, indicated, movie, plataforma)
     @indicator = indicator
     @indicated = indicated
     @movie = movie
     @plataforma = plataforma
     @base_url = "https://teniverdanpose.herokuapp.com/"
-    mail(from: @indicator.email, to: @indicated.email,
-         subject: ('Seu amigo ' + @indicator.nome + ' te indicou um filme' ))
+    mail(to: @indicated.email, subject: ('Seu amigo ' + @indicator.nome + ' te indicou um filme' ))
   end
 end
