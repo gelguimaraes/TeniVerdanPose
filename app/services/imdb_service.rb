e4226f0de0fce4465aaf03de1661ee172dc08fcf
Gesoaldo <gesoaldo.guimaraes@academico.ifpb.edu.br>
2019-08-11 21:01:01 -0300

    mailer

--- app/services/imdb_service.rb
+++ app/services/imdb_service.rb
@@ -1,8 +1,7 @@
 class ImdbService
   def get_plot_by_title(title, year)
     key = ENV["OMBD_KEY"]
-    #a02f04d1
-    response = HTTParty.get("http://www.omdbapi.com/?apikey=#{key}&t=#{title}&y=#{year}")
+    response = HTTParty.get("http://www.omdbapi.com/?apikey=a02f04d1&t=#{title}&y=#{year}")
     response.parsed_response
   end
   def get_plot_by_id(id_imdb)
